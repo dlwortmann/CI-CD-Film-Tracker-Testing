0636a0680e0ff91a4fe805c73d5bd8d8065239d6
dlwortmann <dannywortmann@gmail.com>
2025-05-08 20:41:06 -0500

    home page edit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
     <nav>
       <h1>
         <Link to='/' id='logo'>
-          Film Tracker
+          Film Tracker DODODODOD
         </Link>
       </h1>
       <ul className='nav nav-tabs'>
